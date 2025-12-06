0c5c89fcf0820a26bee82a0af41f442a9f0bc128
VickyAdamPamungkas <adamvicky669@gmail.com>
2025-12-06 01:56:14 +0000

    feat: Implementasi PriorityQueue untuk manajemen tugas

--- Sorting.java
+++ Sorting.java
@@ -1,40 +1,42 @@
 import java.util.ArrayList;
-
 public class Sorting {
 
-    // ================================================
-    // MERGE SORT BY DEADLINE (O(n log n))
-    // ================================================
-    public static void mergeSort(ArrayList<Task> arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
+    // ==============================================
+    // Merge sort by deadline (0(n log n))
+    // ==============================================
+    public static void mergeSort(ArrayList<Task> arr, int kiri, int kanan) {
+        if (kiri < kanan) {
+            int tengah = (kiri + kanan) / 2;
+            
+            // Rekursi
+            mergeSort(arr, kiri, tengah);
+            mergeSort(arr, tengah + 1, kanan);
 
-            // Rekursi → WAJIB dalam tugas
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-
-            merge(arr, left, mid, right);
+            merge(arr, kiri, tengah, kanan);
         }
     }
 
-    private static void merge(ArrayList<Task> arr, int left, int mid, int right) {
+    private static void merge(ArrayList<Task> arr, int kiri, int tengah, int kanan) {
         ArrayList<Task> temp = new ArrayList<>();
+        
+        int i = kiri, j = tengah + 1;
 
-        int i = left, j = mid + 1;
-
-        while (i <= mid && j <= right) {
+        while (i <= tengah && j <= kanan) {
             if (arr.get(i).deadline.compareTo(arr.get(j).deadline) <= 0) {
                 temp.add(arr.get(i++));
+
             } else {
                 temp.add(arr.get(j++));
             }
         }
 
-        while (i <= mid) temp.add(arr.get(i++));
-        while (j <= right) temp.add(arr.get(j++));
+        while (i <= tengah) temp.add(arr.get(i++));
+        while (j <= kanan) temp.add(arr.get(j++));
 
         for (int k = 0; k < temp.size(); k++) {
-            arr.set(left + k, temp.get(k));
+            arr.set(kiri + k, temp.get(k));
+
         }
     }
+    
 }
